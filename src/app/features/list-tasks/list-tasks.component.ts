3623e802dfc9ab303d495a4bd0cb76a888f378e8
Irina Skhirtladze <ira@Skhirtladzes-MacBook-Pro.local>
2023-06-01 14:27:40 +0400

    preliminary commit that went forward to transfering from one category to another

--- src/app/features/list-tasks/list-tasks.component.ts
+++ src/app/features/list-tasks/list-tasks.component.ts
@@ -3,15 +3,17 @@ import { TaskCategory } from 'src/app/store/task-category.enum';
 import { Task } from 'src/app/store/task.interface';
 import { TasksService } from 'src/app/store/tasks.service';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
-import { forkJoin, switchMap } from 'rxjs';
+import { forkJoin } from 'rxjs';
+
 @Component({
   selector: 'app-list-tasks',
   templateUrl: './list-tasks.component.html',
-  styleUrls: ['./list-tasks.component.scss']
+  styleUrls: ['./list-tasks.component.scss'],
 })
 export class ListTasksComponent implements OnInit {
   taskCategories = Object.values(TaskCategory);
   tasksByCategory: { [key: string]: Task[] } = {};
+  connectedDropLists: string[][] = [];
 
   constructor(private taskService: TasksService) {}
 
@@ -20,6 +22,10 @@ export class ListTasksComponent implements OnInit {
     this.fetchTasksByCategory(TaskCategory.NotUrgentNotImportant);
     this.fetchTasksByCategory(TaskCategory.UrgentImportant);
     this.fetchTasksByCategory(TaskCategory.UrgentNotImportant);
+
+    this.connectedDropLists = this.taskCategories.map((category) =>
+      this.taskCategories.filter((c) => c !== category)
+    );
   }
 
   fetchTasksByCategory(category: TaskCategory): void {
@@ -28,43 +34,28 @@ export class ListTasksComponent implements OnInit {
     });
   }
 
-  getTaskCategories(): { key: string; value: string }[] {
-    return Object.entries(this.taskCategories).map(([key, value]) => ({ key, value }));
+  getConnectedDropLists(category: TaskCategory): string[] {
+    return this.connectedDropLists[this.taskCategories.indexOf(category)];
   }
-
-  getTileBackground(index: number): string {
-    const tileColors = ['#B2DFDB', '#FFCCBC', '#BBDEFB', '#FFF9C4'];
-    return tileColors[index % tileColors.length];
-  }
-
-
 
   onDrop(event: CdkDragDrop<Task[]>, category: TaskCategory): void {
     const { previousContainer, container, item } = event;
     const task = item.data;
 
-    console.log("previousContainer", previousContainer.id);
-    console.log("container", container.id);
-
-    if (previousContainer.id === container.id) {
-      // Rearrange the tasks within the same category
+    if (previousContainer === container) {
+      // Rearrange tasks within the same category
       moveItemInArray(container.data, event.previousIndex, event.currentIndex);
-
-      // Update the order IDs of the tasks in the same category
-      container.data.forEach((task, index) => {
-        task.orderId = index + 1; // Adding 1 to start the order ID from 1
-        this.taskService.updateTask(task).subscribe(
-          () => {
-            console.log('Task order updated successfully');
-          },
-          (error) => {
-            console.error('Failed to update task order', error);
-          }
-        );
-      });
     } else {
-      // Remove the task from the previous category
-      previousContainer.data.splice(event.previousIndex, 1);
+      // Move tasks between categories
+      transferArrayItem(
+        previousContainer.data,
+        container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+
+      // Update the category of the task
+      task.categoryId = category;
 
       // Update the order IDs of the tasks in the previous category
       previousContainer.data.forEach((task, index) => {
@@ -78,44 +69,26 @@ export class ListTasksComponent implements OnInit {
           }
         );
       });
+    }
 
-      // Add the task to the new category
-      container.data.splice(event.currentIndex, 0, task);
-      task.categoryId = category;
-      task.orderId = event.currentIndex + 1; // Adding 1 to start the order ID from 1
-
-      // Update the order IDs of the tasks in the new category
-      container.data.forEach((task, index) => {
-        task.orderId = index + 1; // Adding 1 to start the order ID from 1
-        this.taskService.updateTask(task).subscribe(
-          () => {
-            console.log('Task order updated successfully');
-          },
-          (error) => {
-            console.error('Failed to update task order', error);
-          }
-        );
-      });
-
-      // Update the category of the task
+    // Update the order IDs of the tasks in the same/new category
+    container.data.forEach((task, index) => {
+      task.orderId = index + 1; // Adding 1 to start the order ID from 1
       this.taskService.updateTask(task).subscribe(
         () => {
-          console.log('Task category updated successfully');
+          console.log('Task order updated successfully');
         },
         (error) => {
-          console.error('Failed to update task category', error);
+          console.error('Failed to update task order', error);
         }
       );
-    }
+    });
   }
 
 
 
-
-
-
-
-
-
-
+  getTileBackground(index: number): string {
+    const colors = ['lightblue', 'lightgreen', 'lightyellow', 'lightpink'];
+    return colors[index % colors.length];
+  }
 }
